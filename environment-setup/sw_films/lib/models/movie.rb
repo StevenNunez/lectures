92a1150b29e804514262918a3fde0f1adf2ef02e
Steven Nunez <steven.nunez@gmail.com>
2017-02-10 11:43:31 -0500

    Add ::new from api

--- environment-setup/sw_films/lib/models/movie.rb
+++ environment-setup/sw_films/lib/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie
+  def self.new_from_api(movie_hash)
+    title = movie_hash.fetch("title")
+    episode = movie_hash.fetch("episode_id")
+    opening_crawl = movie_hash.fetch("opening_crawl")
+    director = movie_hash.fetch("director")
+    self.new(title: title, episode: episode, opening_crawl: opening_crawl, director: director)
+  end
+
+  attr_reader :title, :episode, :opening_crawl, :director
+  def initialize(title:, episode:, opening_crawl:, director:)
+    @title = title
+    @episode = episode
+    @opening_crawl = opening_crawl
+    @director = director
+  end
 end
